b57015cb3d04eca5004a72e9c2657b80df8c1731
hendraaagil <hendraaagil@gmail.com>
2021-03-04 18:03:04 +0700

    Change Variable Names

--- app/src/main/java/com/imagegallery/ExampleItem.java
+++ app/src/main/java/com/imagegallery/ExampleItem.java
@@ -1,24 +1,24 @@
 package com.imagegallery;
 
 public class ExampleItem {
-    private String mImgUrl, mCreator;
-    private int mLikes;
+    private String imageUrl, creator;
+    private int likes;
 
-    public ExampleItem(String mImgUrl, String mCreator, int mLikes) {
-        this.mImgUrl = mImgUrl;
-        this.mCreator = mCreator;
-        this.mLikes = mLikes;
+    public ExampleItem(String imageUrl, String creator, int likes) {
+        this.imageUrl = imageUrl;
+        this.creator = creator;
+        this.likes = likes;
     }
 
-    public String getmImgUrl() {
-        return mImgUrl;
+    public String getImageUrl() {
+        return imageUrl;
     }
 
-    public String getmCreator() {
-        return mCreator;
+    public String getCreator() {
+        return creator;
     }
 
-    public int getmLikes() {
-        return mLikes;
+    public int getLikes() {
+        return likes;
     }
 }
